7a3f8c97adaa4a3a72f5df77ca19ad216a5bee1e
dozzimeter <145318957+dozzimeter@users.noreply.github.com>
2023-09-22 09:35:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+import requests
+import json
+import os
+
+url = 'http://worldtimeapi.org/api/ip'
+response = requests.request(method='GET', url=url)
+
+x = json.loads(response.text)
+
 f=open('file.out','w')
-f.write('helloworld')
+f.write(os.env.SECRET1)
+json.dump(x, f)
 f.close()
